ee1121a23d833d5ef767c0db00bb1d521e9b48aa
Jasper <jasper@jazper.dk>
2023-02-03 09:31:16 +0100

    Added Movie class

---
+++ TheMovies/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovies.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Director { get; set; }
+        public DateTime Premiere { get; set; }
+    }
+}
