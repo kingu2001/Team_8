cd98b1aefdeb65ee04e9e05cfad588a917da775e
Jasper <jasper@jazper.dk>
2023-02-03 12:45:16 +0100

    Add datahandler

---
+++ TheMovies/Repositories/DataHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheMovies.Models;
+
+namespace TheMovies.Repositories
+{
+    public class DataHandler
+    {
+        public string OrderFilename { get; } = "orderdata.csv";
+        public string CustomerFilename { get; } = "customerdata.csv";
+
+
+        public void Save(Order order)
+        {
+            using (StreamWriter sw = new(OrderFilename))
+            {
+                
+            }
+        }
+
+        public void Save(Customer customer)
+        {
+
+        }
+    }
+}
