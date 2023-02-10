5cb5dd75c844788b02248c90f21a25bb4efbf066
Kiet Nguyen <kietn72@gmail.com>
2023-02-10 08:37:05 +0100

    Filer med persistens

---
+++ Persistens/Persistens/DataHandler.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistens
+{
+    public class DataHandler
+    {
+        private string dataFileName; 
+
+        public string DataFileName
+        {
+            get { return dataFileName; }
+        } 
+
+        public DataHandler(string dataFileName)
+        {
+            this.dataFileName = dataFileName;
+        }
+
+        public void SavePerson(Person person)
+        {
+            using (StreamWriter sw = new StreamWriter(DataFileName))
+            {
+                sw.Write(person.MakeTitle());
+
+            }
+        }
+
+        public Person LoadPerson()
+        {
+            using (StreamReader sr = new StreamReader(DataFileName))
+            {
+                string line = sr.ReadLine();
+                string[] data = line.Split(";");
+                Person person = new Person(data[0], DateTime.Parse(data[1]), double.Parse(data[2]), bool.Parse(data[3]), int.Parse(data[4]));
+                return person;
+            }
+            
+
+        }
+
+        
+        public void SavePersons(Person[] persons)
+        {
+            using (StreamWriter sw = new StreamWriter(DataFileName))
+            {
+
+                for (int i = 0; i < persons.Length; i++)
+                {
+
+                    sw.WriteLine(persons[i].MakeTitle());
+                }
+
+            }
+
+        }
+
+        public Person[] LoadPersons()
+        {
+            using (StreamReader sr = new StreamReader(DataFileName))
+            {
+                string text;
+                Person[] persons = new Person[10];
+                int count = 0;
+
+                while ((text = sr.ReadLine()) != null)
+                {
+                    string[] data = text.Split(";", StringSplitOptions.RemoveEmptyEntries);
+                    persons[count] = new Person(data[0], DateTime.Parse(data[1]), double.Parse(data[2]), bool.Parse(data[3]), int.Parse(data[4]));
+                    count++;
+                }
+                return persons;
+             
+            }
+            
+           
+
+        }
+    }
+}
---
+++ Persistens/Persistens/Person.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistens
+{
+    public class Person
+    {
+        private string name;
+        private DateTime birthdate;
+        private double height;
+        private bool isMarried;
+        private int noOfChildren; 
+
+        public string Name
+        {
+            get { 
+                return name; 
+            }
+            set {
+                name = value;
+                if (name == "")
+                {
+                    throw new Exception("Hej søde ven, du har lavet en fejl:)");
+                }
+            }
+        }
+        public DateTime BirthDate
+        {
+            get { 
+                return birthdate;
+            }
+            set { 
+                birthdate = value;
+                DateTime dt = new DateTime(1900, 1, 1);
+                dt.ToShortDateString();
+                if (birthdate <= dt)
+                {
+                   throw new Exception("Hej søde ven, du har lavet en fejl:)");
+                }
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                height = value;
+                if(height <= 0)
+                {
+                    throw new Exception("Hej søde ven, du har lavet en fejl:)");
+                }
+            }
+        }
+
+        public bool IsMarried
+        {
+            get { return isMarried; }
+            set
+            {
+                isMarried = value;
+            }
+        }
+
+        public int NoOfChildren
+        {
+            get { return noOfChildren; }
+            set
+            {
+                noOfChildren = value;
+                if(noOfChildren < 0)
+                {
+                    throw new Exception("Hej søde ven, du har lavet en fejl:)");
+                }
+            }
+        }
+        
+       
+
+
+        public Person(string name, DateTime birthDate, double height, bool isMarried, int noOfChildren)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            Height = height;
+            IsMarried = isMarried;
+            NoOfChildren = noOfChildren;
+
+
+        }
+
+        public Person(string name, DateTime birthDate, double height, bool isMarried)
+        {
+            this.Name = name;
+            this.BirthDate = birthDate;
+            this.Height = height;
+            this.IsMarried = isMarried;
+            noOfChildren = 0; 
+        }
+        
+
+        public string MakeTitle()
+        {
+            string title = ($"{Name};{BirthDate.ToString(("dd-MM-yyyy HH':'mm':'ss"))};{Height};{IsMarried};{NoOfChildren}");
+            return title; 
+           
+        }
+    }
+}
