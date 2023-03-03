454986881049437444efcdb4b4be8eeb1fe9b6f3
kaer98 <56426532+kaer98@users.noreply.github.com>
2023-03-03 09:08:30 +0100

    make repos

---
+++ LærerVikaren/Persistence/TempHourRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LærerVikaren.Persistence
+{
+    internal class TempHourRepo
+    {
+    }
+}
